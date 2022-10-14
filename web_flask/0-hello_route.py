d853bea7d7060532ba0828c167174d0c8fe6d8ea
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-10-14 06:17:19 -0700

    updating task 0 to listen on port 5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    '''
-    Hello Flask route
-    '''
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
